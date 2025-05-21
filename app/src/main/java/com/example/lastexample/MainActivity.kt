c18013a8919ee71ed7dcae9f1c43cb60fc3fead2
choigeon <qrwrqrwrer@naver.com>
2025-05-21 21:54:49 +0900

    feat: 1번문제 끝

--- app/src/main/java/com/example/lastexample/MainActivity.kt
+++ app/src/main/java/com/example/lastexample/MainActivity.kt
@@ -11,13 +11,47 @@ import com.example.lastexample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+    lateinit var toggle: ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        ViewCompat.setOnApplyWindowInsetsListener() { v, insets ->
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+        toggle = ActionBarDrawerToggle( this, binding.drawer,R.string.menu_open, R.string.menu_close)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        toggle.syncState()
+        title = "기말고사"
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(androidx.core.view.WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        binding.mainDrawerView.setNavigationItemSelectedListener {
+            when (it.itemId){
+                R.id.menu1 -> {
+
+                }
+                R.id.menu2 -> {
+
+                }
+                R.id.menu3 -> {
+
+                }
+                R.id.menu4 -> {
+
+                }
+            }
+            true
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
