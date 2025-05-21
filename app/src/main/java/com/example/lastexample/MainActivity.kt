7659c23e5c7f5c5c47f74fab76620a26505f60bc
choigeon <qrwrqrwrer@naver.com>
2025-05-21 20:05:56 +0900

    feat: 초기화 상태

--- app/src/main/java/com/example/lastexample/MainActivity.kt
+++ app/src/main/java/com/example/lastexample/MainActivity.kt
@@ -11,55 +11,13 @@ import com.example.lastexample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
-    private lateinit var toggle: ActionBarDrawerToggle
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        setSupportActionBar(binding.toolbar)
-        title = "FinalExam"
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        // ActionBarDrawerToggle 설정
-        toggle = ActionBarDrawerToggle(
-            this,
-            binding.drawer,
-            R.string.drawer_opened,
-            R.string.drawer_closed
-        )
-        binding.drawer.addDrawerListener(toggle)
-        toggle.syncState()
-
-        binding.navView.setNavigationItemSelectedListener { menuItem ->
-            when (menuItem.itemId) {
-                R.id.menu1 -> {
-                    Toast.makeText(this, "전화번호부", Toast.LENGTH_SHORT).show()
-                }
-                R.id.menu2 -> {
-                    Toast.makeText(this, "스톱워치", Toast.LENGTH_SHORT).show()
-                }
-                R.id.menu3 -> {
-                    Toast.makeText(this, "일기장", Toast.LENGTH_SHORT).show()
-                }
-                R.id.menu4 -> {
-                    Toast.makeText(this, "뷰 페이저", Toast.LENGTH_SHORT).show()
-                }
-            }
-            binding.drawer.close()
-            true
-        }
-
-        ViewCompat.setOnApplyWindowInsetsListener(binding.drawer) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener() { v, insets ->
             val systemBars = insets.getInsets(androidx.core.view.WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (toggle.onOptionsItemSelected(item)) return true
-        return super.onOptionsItemSelected(item)
     }
 }
